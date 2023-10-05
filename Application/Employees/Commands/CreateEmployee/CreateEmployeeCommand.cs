430caa382e50686b07d5e027c002f3b3b6cf2b51
용국 최 <opzerg9378@gmail.com>
2023-10-05 23:03:46 +0900

    add request, request handler

---
+++ Application/Employees/Commands/CreateEmployee/CreateEmployeeCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Application.Employees.Commands.CreateEmployee;
+
+public record CreateEmployeeCommand : IRequest<int>
+{
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string Tel { get; init; }
+    public DateTime Joined { get; init; }
+}
+
+public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, int>
+{
+    public Task<int> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        // TODO - entity mapping
+        // TODO - create item
+        return Task.FromResult(0);
+    }
+}
--- Domain/Entities/Employee.cs
+++ Domain/Entities/Employee.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Domain.Entities;
 
-namespace Domain.Entities
+public class Employee
 {
-    internal class Employee
-    {
-        public string Name {  get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name {  get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
+
