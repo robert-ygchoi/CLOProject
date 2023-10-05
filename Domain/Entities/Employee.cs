1abc9697a424b2d4db74f0d5cfc958aa8c41c353
용국 최 <opzerg9378@gmail.com>
2023-10-05 22:52:29 +0900

    add employee entity

---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Employee
+    {
+        public string Name {  get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
