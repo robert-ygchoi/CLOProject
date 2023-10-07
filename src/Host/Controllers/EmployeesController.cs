e2c0a1ffaa61d0e39070c2ce3583ddf8e419d908
용국 최 <opzerg9378@gmail.com>
2023-10-07 22:54:19 +0900

    add employeesController

---
+++ src/Host/Controllers/EmployeesController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Employees.Commands.CreateEmployee;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host.Controllers;
+
+public class EmployeesController
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    public async Task<IActionResult> Create(CreateEmployeeCommand command)
+    {
+        throw new NotImplementedException();
+    }
+}
