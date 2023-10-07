03606fd96b4bf4e838f9604167347429a2e1d443
용국 최 <opzerg9378@gmail.com>
2023-10-07 23:16:02 +0900

    define controller and disable https optiosn

--- src/Host/Controllers/EmployeesController.cs
+++ src/Host/Controllers/EmployeesController.cs
@@ -1,16 +1,27 @@
 ﻿using Application.Employees.Commands.CreateEmployee;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Host.Controllers;
 
+[ApiController]
+[Route("api/employee")]
 public class EmployeesController
 {
+    private readonly IMediator _mediator;
+
+    public EmployeesController(IMediator mediator)
+    {
+        this._mediator = mediator;
+    }
+
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesDefaultResponseType]
-    public async Task<IActionResult> Create(CreateEmployeeCommand command)
+    public async Task<IActionResult> Create([FromBody] CreateEmployeeCommand command)
     {
-        throw new NotImplementedException();
+        await this._mediator.Send(command);
+        return new CreatedResult(new Uri("/api/employee", UriKind.Relative), new { name = command.Name });
     }
 }
