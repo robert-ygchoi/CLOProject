e7e1d0d3aed39229f94102653c4c7e8cd468a4aa
용국 최 <opzerg9378@gmail.com>
2023-10-09 01:02:01 +0900

    문자열로 들어왔을 때 구현

--- src/Application/Employees/Commands/ConvertEmployee/ConvertFileEmployeeCommandValidator.cs
+++ src/Application/Employees/Commands/ConvertEmployee/ConvertFileEmployeeCommandValidator.cs
@@ -7,13 +7,13 @@ public class ConvertFileEmployeeCommandValidator : AbstractValidator<ConvertFile
     public ConvertFileEmployeeCommandValidator()
     {
         RuleFor(e => e.JsonOrCsvFileContent)
-#pragma warning disable CS8622 // When 절 때문에 Null이 발생하지 않음.
+#pragma warning disable CS8622 // NOTE: When 절 때문에 Null이 발생하지 않음.
             .Must(BeValidExtension)
 #pragma warning restore CS8622 
             .When(e => e.JsonOrCsvFileContent != null);
     }
 
-    // 확장자가 길어질 경우 refactoring 필요
+    // NOTE: 확장자가 길어질 경우 refactoring 필요
     private static bool BeValidExtension(IFormFile jsonOrCsvFileContent) =>
         jsonOrCsvFileContent.FileName.EndsWith(".csv", StringComparison.OrdinalIgnoreCase) ||
         jsonOrCsvFileContent.FileName.EndsWith(".json", StringComparison.OrdinalIgnoreCase);
---
+++ src/Application/Employees/Commands/ConvertEmployee/ConvertStringEmployeeCommand.cs
@@ -0,0 +1,27 @@
+﻿using Application.Employees.Commands.ParseEmployee;
+
+namespace Application.Employees.Commands.ConvertEmployee;
+
+public record ConvertStringEmployeeCommand(string? Content) : IRequest;
+
+public class ConvertStringEmployeeCommandHandler : IRequestHandler<ConvertStringEmployeeCommand>
+{
+    private readonly IMediator _mediator;
+
+    public ConvertStringEmployeeCommandHandler(IMediator mediator)
+    {
+        this._mediator = mediator;
+    }
+
+    public async Task Handle(ConvertStringEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.Content))
+            return;
+
+        // WARNIGN: 현 과제에서 JsonArray만을 받고 있기 때문에 하기와 검증하는 요소로써 다른 예외가 추가 된다면 refactoring이 필요함
+        if (request.Content.StartsWith("[") && request.Content.EndsWith("]"))
+            await this._mediator.Send(new ParseJsonEmployeeCommand(request.Content));
+        else
+            await this._mediator.Send(new ParseCsvEmployeeCommand(request.Content));
+    }
+}
